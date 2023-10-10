589b842cb63a7675fcb5efb58eaa7bd5e69e61f0
Arthur6533 <128511697+Arthur6533@users.noreply.github.com>
2023-10-10 15:29:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert('não tem som:(')
+function tocasom (idElementoAudio){
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (contador = 0; contador <listaDeTeclas.length; contador++){
+
+    const instrumento = listaDeTeclas[contador].classList[1];
+
+    const tecla = listaDeTeclas[contador];
+
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function (){
+        tocasom(idAudio)
+    }
+
+}
